615a80aa248f2c5aca84b84d6b259bae9aa94ad5
Alexander Walford <74402359+alexanderwalford-official@users.noreply.github.com>
2023-06-14 10:06:17 +0100

    Add project files.

---
+++ Cuda Testing Environment/kernel.cu
@@ -0,0 +1,29 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+
+int main()
+{
+    int a[] = { 1, 2 ,3 };
+    int b[] = { 4, 5, 6 };
+    int c[sizeof(a) / sizeof(int)] = { 0 };
+
+    int* cudaA = 0;
+    int* cudaB = 0;
+    int* cudaC = 0;
+
+    cudaMalloc(&cudaA, sizeof(a));
+    cudaMalloc(&cudaB, sizeof(b));
+    cudaMalloc(&cudaC, sizeof(c));
+
+    for (int i = 0; i < sizeof(c) / sizeof(int); i++) {
+        c[i] = a[i] + b[i];
+    }
+
+    printf("Compiled on GPU.");
+
+    return;
+}
